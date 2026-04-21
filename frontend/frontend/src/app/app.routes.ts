35bcadd0c67f2fcb83f4177488ac2d78c3de4731
hannah.schneider <hannah.schneider@hmmh.de>
2026-04-21 19:09:43 +0200

    feat(user): add change-password component and generic-button (NEBO-103/104)

--- frontend/frontend/src/app/app.routes.ts
+++ frontend/frontend/src/app/app.routes.ts
@@ -1,9 +1,10 @@
 import { Routes } from '@angular/router';
+import {ChangePassword} from './change-password/change-password';
 
 export const routes: Routes = [
   {
     path: 'profile',
     loadComponent: () =>
       import('./profile/profile').then(m => m.Profile)
-  }
+  },{path: "profile/:id/change-password", component: ChangePassword}
 ];
