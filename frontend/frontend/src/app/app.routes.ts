504bb8cd42f881a6abd91497d1842a4708cd3a77
hannah.schneider <hannah.schneider@hmmh.de>
2026-04-23 10:47:35 +0200

    feat(verification): add email verification page (NEBO-97)

--- frontend/frontend/src/app/app.routes.ts
+++ frontend/frontend/src/app/app.routes.ts
@@ -1,22 +1,25 @@
-import { Routes} from '@angular/router';
-
-
+import { Routes } from '@angular/router';
+import { VerifyEmail } from './verify-email/verify-email';
+import { Profile } from './profile/profile';
 
 export const routes: Routes = [
   {
     path: '',
     redirectTo: 'auth',
-    pathMatch: 'full'
+    pathMatch: 'full',
   },
   {
     path: 'auth',
     loadComponent: () =>
-      import('./Authentification/Auth-Page/auth-page')
-        .then(m => m.AuthPageComponent)
+      import('./Authentification/Auth-Page/auth-page').then((m) => m.AuthPageComponent),
   },
   {
     path: 'profile',
-    loadComponent: () =>
-      import('./profile/profile').then(m => m.Profile)
-  }
+    loadComponent: () => import('./profile/profile').then((m) => m.Profile),
+  },
+  {
+    path: 'verify-email',
+    component: VerifyEmail,
+  },
+  // {path: "profile/login", component: AuthPage}
 ];
---
+++ frontend/frontend/src/app/verify-email/verify-email.ts
@@ -0,0 +1,49 @@
+import { Component, OnInit } from '@angular/core';
+import {ActivatedRoute, RouterLink, RouterModule} from '@angular/router';
+import { HttpErrorResponse } from '@angular/common/http';
+import { VerifyEmailService } from  "../service/verify-email-service"
+
+
+@Component({
+  selector: 'app-verify-email',
+  templateUrl: './verify-email.html',
+  imports: [
+    RouterLink, RouterModule
+  ],
+  styleUrls: ['./verify-email.css']
+})
+export class VerifyEmail implements OnInit {
+  message = '';
+  errorMessage = '';
+
+  constructor(
+    private route: ActivatedRoute,
+    private verifyEmailService: VerifyEmailService
+  ) {}
+
+  ngOnInit(): void {
+    const token = this.route.snapshot.queryParamMap.get('token');
+
+    if (!token) {
+      this.errorMessage = 'Kein Token gefunden.';
+      return;
+    }
+
+    this.verifyEmailService.verifyEmail(token).subscribe({
+      next: (response: string) => {
+        this.message = response;
+        this.errorMessage = '';
+      },
+      error: (error: HttpErrorResponse) => {
+        console.log(error);
+
+        if (typeof error.error === 'string') {
+          this.errorMessage = error.error;
+        } else {
+          this.errorMessage =
+            error.error?.message || 'Ein unbekannter Fehler ist aufgetreten.';
+        }
+      },
+    });
+  }
+}
