1279c45aea84cf90b045e1900a26954a38fb5c8d
hannah.schneider <hannah.schneider@hmmh.de>
2026-04-24 15:21:28 +0200

    chore(password): remove custom token logic

--- frontend/frontend/src/app/service/user-service.ts
+++ frontend/frontend/src/app/service/user-service.ts
@@ -1,6 +1,5 @@
 import {Injectable} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
-import {HttpHeaders} from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root',
@@ -8,23 +7,13 @@ import {HttpHeaders} from '@angular/common/http';
 export class UserService {
   private baseUrl = 'http://localhost:8080/api/users';
 
-  constructor(private http: HttpClient) {
-  };
-
+  constructor(private http: HttpClient) {}
 
   submitPasswords(data: { oldPassword: string; newPassword: string }, userId: string) {
-    const token = localStorage.getItem('token');
-
-    const headers = new HttpHeaders({
-      Authorization: `Bearer ${token}`,
-      'Content-Type': 'application/json'
-    });
-
     return this.http.put(
       `${this.baseUrl}/${userId}/change-password`,
       data,
       {
-        headers,
         responseType: 'text'
       }
     );
