6343411cba6e4c671de585545b88a3fb60e73905
ZAKARIA SIRWAN <zakariazz2009@hotmail.com>
2026-04-29 15:50:38 +0200

    NEBO-113 load and display profile data

--- frontend/frontend/src/app/profile/profile.ts
+++ frontend/frontend/src/app/profile/profile.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
-import {AccountDeleteArea} from '../account-delete-area/account-delete-area';
+import { Component, inject, signal } from '@angular/core';
 import {Rating} from '../rating/rating';
+import { AccountDeleteArea } from '../account-delete-area/account-delete-area';
+import { ProfileService, ProfileData } from '../Service/profile.service';
 
 @Component({
   selector: 'app-profile',
@@ -9,5 +10,14 @@ import {Rating} from '../rating/rating';
   styleUrl: './profile.css',
 })
 export class Profile {
+  private profileService = inject(ProfileService);
 
+  profile = signal<ProfileData | null>(null);
+
+  constructor() {
+    this.profileService.getProfile().subscribe({
+      next: (data) => this.profile.set(data),
+      error: (err) => console.error('Fehler beim Laden des Profils', err),
+    });
+  }
 }
