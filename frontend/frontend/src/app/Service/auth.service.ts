b7534347549767ce7cf2baea44319c3750ae086d
AkramAmmis <akramammis0@gamil.com>
2026-04-30 11:27:06 +0200

    Add token management and user authentication methods

--- frontend/frontend/src/app/Service/auth.service.ts
+++ frontend/frontend/src/app/Service/auth.service.ts
@@ -1,6 +1,7 @@
 import { Injectable, inject } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { tap } from 'rxjs';
+import {Router} from '@angular/router';
 
 export interface LoginRequest {
   email: string;
@@ -19,6 +20,8 @@ export interface RegisterRequest {
 export class AuthService {
   private readonly http = inject(HttpClient);
   private readonly apiUrl = '/api/auth';
+  private readonly TOKEN_KEY = 'auth_token';
+  private router = inject(Router);
 
   login(data: LoginRequest) {
     return this.http.post(`${this.apiUrl}/login`, data, { responseType: 'text' }).pipe(
@@ -28,5 +31,21 @@ export class AuthService {
 
   register(data: RegisterRequest) {
     return this.http.post(`${this.apiUrl}/register`, data, { responseType: 'text' });
+  }
+  saveToken(token: string): void {
+    localStorage.setItem(this.TOKEN_KEY, token);
+  }
+
+  getToken(): string | null {
+    return localStorage.getItem(this.TOKEN_KEY);
+  }
+
+  logout(): void {
+    localStorage.removeItem(this.TOKEN_KEY);
+    this.router.navigate(['/login']);
+  }
+
+  isLoggedIn(): boolean {
+    return !!this.getToken();
   }
 }
