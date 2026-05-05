faa7b355ac2e48a57eaa2659bca74570da7fe5aa
Ranim Alsalloum <ranim.alsalloum@hmmh.de>
2026-05-05 12:42:29 +0200

    NEBO-293: API-Contract für Create Post vorbereiten

---
+++ backend/src/main/java/de/neighbourly/backend/dto/CreatePostRequest.java
@@ -0,0 +1,20 @@
+package de.neighbourly.backend.dto;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@AllArgsConstructor
+public class CreatePostRequest {
+
+    private Long id;
+    private String title;
+    private String description;
+    private String type;
+    private boolean isUrgent;
+    private LocalDateTime urgentUntil;
+    private LocalDateTime createdAt;
+}
