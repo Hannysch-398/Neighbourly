e14d946cb54c9ea8f61344c1ff75a63247b18607
dylan.schwarz <dylan.schwarz@hmmh.de>
2026-05-05 16:18:20 +0200

    rollback to the state before the merge

--- frontend/frontend/src/app/profile/profile.ts
+++ frontend/frontend/src/app/profile/profile.ts
@@ -1,23 +1,10 @@
-import { Component, inject, signal } from '@angular/core';
-import { AccountDeleteArea } from '../account-delete-area/account-delete-area';
-import { ProfileService, ProfileData } from '../Service/profile.service';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-profile',
   standalone: true,
-  imports: [AccountDeleteArea],
+  imports: [],
   templateUrl: './profile.html',
   styleUrl: './profile.css',
 })
-export class Profile {
-  private profileService = inject(ProfileService);
-
-  profile = signal<ProfileData | null>(null);
-
-  constructor() {
-    this.profileService.getProfile().subscribe({
-      next: (data) => this.profile.set(data),
-      error: (err) => console.error('Fehler beim Laden des Profils', err),
-    });
-  }
-}
+export class Profile {}
