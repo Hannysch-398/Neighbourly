624636758a74415c95e01158ad602d4c649551cd
ZAKARIA SIRWAN <zakariazz2009@hotmail.com>
2026-05-11 12:45:20 +0200

    NEBO-165 make rating migration idempotent

--- backend/src/main/resources/db/migration/V5__create_rating_table.sql
+++ backend/src/main/resources/db/migration/V5__create_rating_table.sql
@@ -1,9 +1,8 @@
-CREATE TABLE rating
-(
-    id            BIGSERIAL PRIMARY KEY,
-    rater_user_id BIGINT  NOT NULL,
-    rated_user_id BIGINT  NOT NULL,
-    rating        INTEGER NOT NULL,
-    comment       VARCHAR(255),
+CREATE TABLE IF NOT EXISTS rating (
+       id BIGSERIAL PRIMARY KEY,
+       rater_user_id BIGINT NOT NULL,
+       rated_user_id BIGINT NOT NULL,
+       rating INTEGER NOT NULL,
+       comment VARCHAR(255),
     creation_date TIMESTAMP
-);
+    );
