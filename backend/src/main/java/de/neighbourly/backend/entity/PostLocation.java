88a75e775ed4b39a60b4bb7925b256ce95cf778d
Ranim Alsalloum <ranim.alsalloum@hmmh.de>
2026-05-13 19:48:23 +0200

    NEBO-167 Standort, Tags und Bilder in Detail-Response integriert

---
+++ backend/src/main/java/de/neighbourly/backend/entity/PostLocation.java
@@ -0,0 +1,30 @@
+package de.neighbourly.backend.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "post_locations")
+@Getter
+@Setter
+public class PostLocation {
+        @Id
+        @GeneratedValue(strategy = GenerationType.IDENTITY)
+        private Long id;
+
+        private String city;
+
+        private String district;
+
+        private Double latitude;
+
+        private Double longitude;
+
+        @OneToOne
+        @JoinColumn(name = "post_id", nullable = false)
+        private Post post;
+
+}
