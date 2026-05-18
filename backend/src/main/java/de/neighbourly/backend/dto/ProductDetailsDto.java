bb52880e562a5b3e4131cd03022ff4ffbdd53476
taha-yasin.ipek <taha-yasin.ipek@hmmh.de>
2026-05-18 12:24:22 +0200

    forgot to add ProductDetailsDto

--- backend/src/main/java/de/neighbourly/backend/dto/ProductDetailsDto.java
+++ backend/src/main/java/de/neighbourly/backend/dto/ProductDetailsDto.java
@@ -2,14 +2,19 @@ package de.neighbourly.backend.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.math.BigDecimal;
 
 @Getter
 @Setter
+@NoArgsConstructor
 @AllArgsConstructor
-public class ProductDetailsDto {
+public class ProductDetailsDto implements PostDetailsDto {
+
+    private String detailType;
+
     private String productName;
     private BigDecimal price;
 }
