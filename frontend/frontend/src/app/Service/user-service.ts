4c55377b2c2d7df8400bf542944c8f2d9a8454f2
dylan.schwarz <dylan.schwarz@hmmh.de>
2026-05-19 10:11:27 +0200

    deleted imports of signal and computed

--- frontend/frontend/src/app/Service/user-service.ts
+++ frontend/frontend/src/app/Service/user-service.ts
@@ -1,4 +1,4 @@
-import { Injectable, signal, computed } from '@angular/core';
+import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { toSignal } from '@angular/core/rxjs-interop';
 import { map } from 'rxjs';
