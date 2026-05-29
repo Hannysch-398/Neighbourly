86248c88be39c16eeea9b0a0fd164a53509f98bc
AkramAmmis <akramammis0@gamil.com>
2026-05-29 17:14:14 +0200

    Add UpdatePostRequest interface and updatePost method

--- frontend/frontend/src/app/service/posts.service.ts
+++ frontend/frontend/src/app/service/posts.service.ts
@@ -5,6 +5,7 @@ import {postListMock} from '../mocks/post.mock';
 import {MapPostMarker} from '../interface/MapPostMarker';
 import {MOCK_MAP_POST_MARKERS} from '../mocks/mapPost.mock';
 import { CreatePostRequest, PostResponse } from '../models/post.model';
+import {UpdatePostRequest} from '../interface/UpdatePostRequest';
 
 
 @Injectable({
@@ -44,6 +45,10 @@ export class PostsService {
 
   createPost(payload: CreatePostRequest): Observable<PostResponse> {
     return this.http.post<PostResponse>(this.apiUrl, payload);
+  }
+
+  updatePost(id: number, payload: UpdatePostRequest): Observable<PostResponse> {
+    return this.http.put<PostResponse>(`${this.apiUrl}/${id}`, payload);
   }
 
 }
