d58e30e12112546e9b2735e33fe39d4e2e5f5d41
hannah.schneider <hannah.schneider@hmmh.de>
2026-05-29 11:22:46 +0200

    feat: use existing Type and update Mockdata

--- frontend/frontend/src/app/mocks/mapPost.mock.ts
+++ frontend/frontend/src/app/mocks/mapPost.mock.ts
@@ -7,8 +7,13 @@ export const MOCK_MAP_POST_MARKERS: MapPostMarker[] = [
     title: 'Nachbarschaftstreffen',
     lat: 53.0889,
     lng: 8.7961,
+    postMode: 'REQUEST',
     isUrgent: true,
     createdAt: '2026-05-07T10:00:00Z',
+    shortDescription: 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt' +
+      ' ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea ' +
+      'rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit ' +
+      'amet, consetetur sadipscing elitr, sed diam.'
   },
   {
     id: 2,
@@ -16,8 +21,13 @@ export const MOCK_MAP_POST_MARKERS: MapPostMarker[] = [
     title: 'Biete Fahrradreparatur',
     lat: 53.0878,
     lng: 8.7972,
+    postMode: "REQUEST",
     isUrgent: false,
     createdAt: '2026-05-07T10:15:00Z',
+    shortDescription: 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt' +
+      ' ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea ' +
+      'rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit ' +
+      'amet, consetetur sadipscing elitr, sed diam.'
   },
   {
     id: 3,
@@ -25,8 +35,13 @@ export const MOCK_MAP_POST_MARKERS: MapPostMarker[] = [
     title: 'Werkzeug zu verschenken',
     lat: 53.0894,
     lng: 8.7939,
+    postMode: "REQUEST",
     isUrgent: false,
     createdAt: '2026-05-07T10:30:00Z',
+    shortDescription: 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt' +
+      ' ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea ' +
+      'rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit ' +
+      'amet, consetetur sadipscing elitr, sed diam.'
   },
   {
     id: 4,
@@ -34,16 +49,26 @@ export const MOCK_MAP_POST_MARKERS: MapPostMarker[] = [
     title: 'Zimmer kurzfristig gesucht',
     lat: 53.0869,
     lng: 8.7984,
+    postMode: "REQUEST",
     isUrgent: true,
     createdAt: '2026-05-07T10:45:00Z',
+    shortDescription: 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt' +
+      ' ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea ' +
+      'rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit ' +
+      'amet, consetetur sadipscing elitr, sed diam.'
   },
   {
     id: 5,
-    type: '',
+    type: 'HOUSING',
     title: 'Zimmer kurzfristig gesucht',
     lat: 53.0869,
     lng: 8.7984,
+    postMode: "REQUEST",
     isUrgent: true,
     createdAt: '2026-05-07T10:45:00Z',
+    shortDescription: 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt' +
+      ' ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea ' +
+      'rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit ' +
+      'amet, consetetur sadipscing elitr, sed diam.'
   }
 ];
