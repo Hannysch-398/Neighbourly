6e22f996900ab3f50197bb5b24e9102e3e12d672
hannah.schneider <hannah.schneider@hmmh.de>
2026-05-31 13:55:33 +0200

    feat: sidebar entries

--- frontend/frontend/src/app/map-overlay/map-overlay.ts
+++ frontend/frontend/src/app/map-overlay/map-overlay.ts
@@ -1,12 +1,41 @@
-import {Component, EventEmitter, Input, Output} from '@angular/core';
+import {
+  Component,
+  EventEmitter,
+  Input,
+  OnInit,
+  Output,
+  signal,
+  WritableSignal,
+} from '@angular/core';
+
+import { SidebarEntry } from '../sidebar-entry/sidebar-entry';
+import { MapPostMarker } from '../interface/MapPostMarker';
+import { PostsService as PostService } from '../services/posts.service';
 
 @Component({
   selector: 'app-map-overlay',
   standalone: true,
   templateUrl: './map-overlay.html',
   styleUrls: ['./map-overlay.css'],
+  imports: [SidebarEntry],
 })
-export class MapOverlayComponent {
+export class MapOverlayComponent implements OnInit {
   @Input() isOpen = false;
   @Output() toggleOverlay = new EventEmitter<void>();
+
+  posts: WritableSignal<MapPostMarker[]> = signal([]);
+
+  constructor(private readonly postService: PostService) {}
+
+  ngOnInit(): void {
+    this.loadSidebarEntries();
+  }
+
+  private loadSidebarEntries(): void {
+    this.postService
+      .getMapPostMarker(0, 0, 0)
+      .subscribe((posts) => {
+        this.posts.set(posts);
+      });
+  }
 }
