c663a3e72f8fb79b022fde9f0f9287d0f00821f8
ZAKARIA SIRWAN <zakariazz2009@hotmail.com>
2026-06-02 17:47:32 +0200

    NEBO-230 add urgent help section

--- frontend/frontend/src/app/posts-list/posts-list.ts
+++ frontend/frontend/src/app/posts-list/posts-list.ts
@@ -19,6 +19,18 @@ export class PostsListComponent implements OnInit {
   readonly isLoading = signal(true);
   readonly errorMessage = signal('');
   readonly hasPosts = computed(() => this.posts().length > 0);
+  readonly urgentPosts = computed(() =>
+    this.posts()
+      .filter((post) => post.isUrgent)
+      .slice(0, 3)
+  );
+
+  readonly regularPosts = computed(() => {
+    const pinnedIds = new Set(this.urgentPosts().map((post) => post.id));
+
+    return this.posts().filter((post) => !pinnedIds.has(post.id));
+  });
+
 
   ngOnInit(): void {
     this.loadPosts();
@@ -63,4 +75,6 @@ export class PostsListComponent implements OnInit {
   trackPostById(_: number, post: PostResponse): number {
     return post.id;
   }
+
+
 }
